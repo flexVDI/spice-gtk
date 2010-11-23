d960229a091a7bd5b3ce4a29ae5f5648978af51c
Marc-André Lureau <marcandre.lureau@gmail.com>
2010-11-23 17:00:17 +0100

    Initial import from SPICE

---
+++ autogen.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -e # exit on errors
+
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
+
+autoreconf -v --force --install || exit 1
+
+if [ -z "$NOCONFIGURE" ]; then
+    "$srcdir"/configure --enable-maintainer-mode ${1+"$@"} || exit 1
+fi
+
