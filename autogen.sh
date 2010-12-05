165000de77f0e334181a4cf1f613a0a1d4898f3d
Marc-André Lureau <marcandre.lureau@redhat.com>
2010-12-05 22:16:35 +0100

    gtk: first stab at gtk-doc documentation

--- autogen.sh
+++ autogen.sh
@@ -5,10 +5,11 @@ set -e # exit on errors
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
-autoreconf -v --force --install || exit 1
+gtkdocize
+autoreconf -v --force --install
 intltoolize -f
 
 if [ -z "$NOCONFIGURE" ]; then
-    "$srcdir"/configure --enable-maintainer-mode ${1+"$@"} || exit 1
+    "$srcdir"/configure --enable-maintainer-mode ${1+"$@"}
 fi
 
