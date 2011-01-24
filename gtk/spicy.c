6899c62b825a7f74a904002ceb90031dd43a5117
Marc-André Lureau <marcandre.lureau@redhat.com>
2011-01-24 12:58:41 +0100

    gtk: spicy: display connection dialog on TLS error

--- gtk/spicy.c
+++ gtk/spicy.c
@@ -798,10 +798,10 @@ static void main_channel_event(SpiceChannel *channel, SpiceChannelEvent event,
         g_message("main channel: closed");
         connection_disconnect(conn);
         break;
-    case SPICE_CHANNEL_ERROR_TLS:
     case SPICE_CHANNEL_ERROR_IO:
         connection_disconnect(conn);
         break;
+    case SPICE_CHANNEL_ERROR_TLS:
     case SPICE_CHANNEL_ERROR_LINK:
     case SPICE_CHANNEL_ERROR_CONNECT:
         g_message("main channel: failed to connect");
