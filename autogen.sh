3f54948390f4258b27b106cf7f58b71f02a08978
Marc-André Lureau <marcandre.lureau@redhat.com>
2011-03-30 17:29:42 +0200

    build fixup

--- autogen.sh
+++ autogen.sh
@@ -8,8 +8,11 @@ test -z "$srcdir" && srcdir=.
 gtkdocize
 autoreconf -v --force --install
 intltoolize -f
+if test ! -e gtk/controller/controller.stamp; then
+  enable_vala="--enable-vala"
+fi
 
 if [ -z "$NOCONFIGURE" ]; then
-    "$srcdir"/configure --enable-maintainer-mode ${1+"$@"}
+    "$srcdir"/configure --enable-maintainer-mode $enable_vala ${1+"$@"}
 fi
 
