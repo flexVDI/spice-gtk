6871b8dfa1ea7dac5e0a75cfaeac7e56548ed30f
Christophe Fergeau <cfergeau@redhat.com>
2011-05-05 19:22:21 +0200

    add basic callbacks for smartcard events
    Now that the smartcard manager sends event for smartcard readers,
    the smartcard channel has to do something with them. For now, we just
    add basic callbacks which don't try to forward these events to the
    spice server. However, we start adding the glue needed to handle
    the fact that plugging of smartcard readers and creation of these
    readers in the spice server will be asynchronous.
    After receiving a "reader addition" command, the spice server sends
    back an ID to identify this reader which must be used in all other
    requests (card insertion, removal and reader removal) to identify
    this reader. However, if the server is slow to send back this ID,
    there's a window when we can get additional events for the reader
    that is being added that we can't send right away to the server
    because we don't know the ID to use. That's why we add the various
    pending_* hashes in this commit, to be able to keep track of the
    requests that will have to be sent once the spice server has
    assigned an ID to the reader.

--- gtk/channel-smartcard.c
+++ gtk/channel-smartcard.c
@@ -15,12 +15,13 @@
    You should have received a copy of the GNU Lesser General Public
    License along with this library; if not, see <http://www.gnu.org/licenses/>.
 */
+#include <vreader.h>
+
 #include "spice-client.h"
 #include "spice-common.h"
 
 #include "spice-channel-priv.h"
-#include "spice-channel-cache.h"
-#include "spice-marshal.h"
+#include "smartcard-manager.h"
 
 /**
  * SECTION:channel-smartcard
@@ -38,6 +39,20 @@
     (G_TYPE_INSTANCE_GET_PRIVATE((obj), SPICE_TYPE_SMARTCARD_CHANNEL, spice_smartcard_channel))
 
 struct spice_smartcard_channel {
+    /* track readers that have been added but for which we didn't receive
+     * an ack from the spice server yet. We rely on the fact that the
+     * readers in this list are ordered by the time we sent the request to
+     * the server. When we get an ack from the server for a reader addition,
+     * we can pop the 1st entry to get the reader the ack corresponds to. */
+    GList *pending_reader_additions;
+
+    /* used to removals of readers that were not ack'ed yet by the spice
+     * server */
+    GHashTable *pending_reader_removals;
+
+    /* used to track card insertions on readers that were not ack'ed yet
+     * by the spice server */
+    GHashTable *pending_card_insertions;
 };
 
 G_DEFINE_TYPE(SpiceSmartCardChannel, spice_smartcard_channel, SPICE_TYPE_CHANNEL)
@@ -51,13 +66,54 @@ static void spice_smartcard_handle_msg(SpiceChannel *channel, spice_msg_in *msg)
 
 /* ------------------------------------------------------------------ */
 
+static void reader_added_cb(SpiceSmartCardManager *manager, VReader *reader,
+                            gpointer user_data);
+static void reader_removed_cb(SpiceSmartCardManager *manager, VReader *reader,
+                              gpointer user_data);
+static void card_inserted_cb(SpiceSmartCardManager *manager, VReader *reader,
+                             gpointer user_data);
+static void card_removed_cb(SpiceSmartCardManager *manager, VReader *reader,
+                            gpointer user_data);
+
 static void spice_smartcard_channel_init(SpiceSmartCardChannel *channel)
 {
+    SpiceSmartCardManager *manager;
+    spice_smartcard_channel *priv;
+
     channel->priv = SPICE_SMARTCARD_CHANNEL_GET_PRIVATE(channel);
+    priv = channel->priv;
+    priv->pending_card_insertions =
+        g_hash_table_new_full(g_direct_hash, g_direct_equal,
+                              (GDestroyNotify)vreader_free, NULL);
+    priv->pending_reader_removals =
+         g_hash_table_new_full(g_direct_hash, g_direct_equal,
+                               (GDestroyNotify)vreader_free, NULL);
+
+    manager = spice_smartcard_manager_get();
+    g_signal_connect(G_OBJECT(manager), "reader-added",
+                     (GCallback)reader_added_cb, channel);
+    g_signal_connect(G_OBJECT(manager), "reader-removed",
+                     (GCallback)reader_removed_cb, channel);
+    g_signal_connect(G_OBJECT(manager), "card-inserted",
+                     (GCallback)card_inserted_cb, channel);
+    g_signal_connect(G_OBJECT(manager), "card-removed",
+                     (GCallback)card_removed_cb, channel);
 }
 
 static void spice_smartcard_channel_finalize(GObject *obj)
 {
+    SpiceSmartCardChannel *channel = SPICE_SMARTCARD_CHANNEL(obj);
+
+    if (channel->priv->pending_card_insertions != NULL) {
+        g_hash_table_destroy(channel->priv->pending_card_insertions);
+        channel->priv->pending_card_insertions = NULL;
+    }
+
+    if (channel->priv->pending_reader_removals != NULL) {
+        g_hash_table_destroy(channel->priv->pending_reader_removals);
+        channel->priv->pending_reader_removals = NULL;
+    }
+
     if (G_OBJECT_CLASS(spice_smartcard_channel_parent_class)->finalize)
         G_OBJECT_CLASS(spice_smartcard_channel_parent_class)->finalize(obj);
 }
@@ -76,6 +132,105 @@ static void spice_smartcard_channel_class_init(SpiceSmartCardChannelClass *klass
 static const spice_msg_handler smartcard_handlers[] = {
     [ SPICE_MSG_SMARTCARD_DATA ]           = NULL,
 };
+
+/* ------------------------------------------------------------------ */
+/* private api                                                        */
+static gboolean is_attached_to_server(VReader *reader)
+{
+    return (vreader_get_id(reader) != (vreader_id_t)-1);
+}
+
+G_GNUC_UNUSED static gboolean
+spice_channel_has_pending_card_insertion(SpiceSmartCardChannel *channel,
+                                         VReader *reader)
+{
+    return (g_hash_table_lookup(channel->priv->pending_card_insertions, reader) != NULL);
+}
+
+static void
+spice_channel_queue_card_insertion(SpiceSmartCardChannel *channel,
+                                   VReader *reader)
+{
+    vreader_reference(reader);
+    g_hash_table_insert(channel->priv->pending_card_insertions,
+                        reader, reader);
+}
+
+static void
+spice_channel_drop_pending_card_insertion(SpiceSmartCardChannel *channel,
+                                          VReader *reader)
+{
+    g_hash_table_remove(channel->priv->pending_card_insertions, reader);
+}
+
+G_GNUC_UNUSED static gboolean
+spice_channel_has_pending_reader_removal(SpiceSmartCardChannel *channel,
+                                         VReader *reader)
+{
+    return (g_hash_table_lookup(channel->priv->pending_reader_removals, reader) != NULL);
+}
+
+static void
+spice_channel_queue_reader_removal(SpiceSmartCardChannel *channel,
+                                   VReader *reader)
+{
+    vreader_reference(reader);
+    g_hash_table_insert(channel->priv->pending_reader_removals,
+                        reader, reader);
+}
+
+G_GNUC_UNUSED static void
+spice_channel_drop_pending_reader_removal(SpiceSmartCardChannel *channel,
+                                          VReader *reader)
+{
+    g_hash_table_remove(channel->priv->pending_reader_removals, reader);
+}
+
+
+static void reader_added_cb(SpiceSmartCardManager *manager, VReader *reader,
+                            gpointer user_data)
+{
+    SpiceSmartCardChannel *channel = SPICE_SMARTCARD_CHANNEL(user_data);
+
+    channel->priv->pending_reader_additions =
+        g_list_append(channel->priv->pending_reader_additions, reader);
+}
+
+static void reader_removed_cb(SpiceSmartCardManager *manager, VReader *reader,
+                              gpointer user_data)
+{
+    SpiceSmartCardChannel *channel = SPICE_SMARTCARD_CHANNEL(user_data);
+
+    if (is_attached_to_server(reader)) {
+    } else {
+        spice_channel_queue_reader_removal(channel, reader);
+    }
+}
+
+/* ------------------------------------------------------------------ */
+/* callbacks                                                          */
+static void card_inserted_cb(SpiceSmartCardManager *manager, VReader *reader,
+                             gpointer user_data)
+{
+    SpiceSmartCardChannel *channel = SPICE_SMARTCARD_CHANNEL(user_data);
+
+    if (is_attached_to_server(reader)) {
+    } else {
+        spice_channel_queue_card_insertion(channel, reader);
+    }
+}
+
+static void card_removed_cb(SpiceSmartCardManager *manager, VReader *reader,
+                            gpointer user_data)
+{
+    SpiceSmartCardChannel *channel = SPICE_SMARTCARD_CHANNEL(user_data);
+
+    if (is_attached_to_server(reader)) {
+    } else {
+        /* this does nothing when reader has no card insertion pending */
+        spice_channel_drop_pending_card_insertion(channel, reader);
+    }
+}
 
 /* coroutine context */
 static void spice_smartcard_handle_msg(SpiceChannel *channel, spice_msg_in *msg)
