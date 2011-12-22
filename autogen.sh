c5f42f8fb31c371548107ddf4114115d0e269946
Christophe Fergeau <cfergeau@redhat.com>
2011-12-22 12:53:44 +0100

    Disable vala bindings unless --enable-vala is used
    Currently, building vala bindings from a tarball is broken because
    spice-client-glib-2.0.deps is missing from the tarball. This commit
    adds it to EXTRA_DIST and also makes sure the vala bindings don't
    get built/installed unless --enable-vala has been passed to configure.
    This means vala must be installed to build the vala bindings from a
    tarball. Fixes fdo bug #44000.

--- autogen.sh
+++ autogen.sh
@@ -13,6 +13,6 @@ if test ! -e gtk/controller/controller.vala.stamp; then
 fi
 
 if [ -z "$NOCONFIGURE" ]; then
-    "$srcdir"/configure --enable-maintainer-mode $enable_vala ${1+"$@"}
+    "$srcdir"/configure --enable-maintainer-mode --enable-gtk-doc --with-gtk=3.0 $enable_vala ${1+"$@"}
 fi
 
