79436bcd0d44eecef447bac23d8ea4eb4c81fb78
Christophe Fergeau <cfergeau@redhat.com>
2012-04-02 12:05:48 +0200

    autogen.sh: log configure command line
    Since --enable-vala may or may not be passed to configure, seeing
    the actual command line that was used is helpful.

--- autogen.sh
+++ autogen.sh
@@ -15,6 +15,7 @@ if test ! -e gtk/controller/controller.vala.stamp; then
 fi
 
 if [ -z "$NOCONFIGURE" ]; then
+    echo "Running configure with --enable-maintainer-mode --enable-gtk-doc --with-gtk=3.0 $enable_vala ${1+"$@"}"
     "$srcdir"/configure --enable-maintainer-mode --enable-gtk-doc --with-gtk=3.0 $enable_vala ${1+"$@"}
 fi
 
