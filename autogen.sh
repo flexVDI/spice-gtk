23f4cc040fae0579068bb00e689fbab4119a15dd
Christophe Fergeau <cfergeau@redhat.com>
2012-04-04 10:29:04 +0200

    autogen.sh: default to --enable-vala when building from git
    People using autogen.sh are likely to be building from git, so may
    get updates to vala files at any time. Checking for the presence of
    controller.vala.stamp to decide whether vala should be enabled or not
    is not very accurate since it doesn't reflect if a .vala file needs
    to be regenerated or not.
    It's better to always pass --enable-vala to configure, it's always
    possible to disable it by using --disable-vala as an autogen.sh argument.

--- autogen.sh
+++ autogen.sh
@@ -10,12 +10,9 @@ git submodule update --init --recursive
 gtkdocize
 autoreconf -v --force --install
 intltoolize -f
-if test ! -e gtk/controller/controller.vala.stamp; then
-  enable_vala="--enable-vala"
-fi
 
 if [ -z "$NOCONFIGURE" ]; then
-    echo "Running configure with --enable-maintainer-mode --enable-gtk-doc --with-gtk=3.0 $enable_vala ${1+"$@"}"
-    "$srcdir"/configure --enable-maintainer-mode --enable-gtk-doc --with-gtk=3.0 $enable_vala ${1+"$@"}
+    echo "Running configure with --enable-maintainer-mode --enable-gtk-doc --with-gtk=3.0 --enable-vala ${1+"$@"}"
+    "$srcdir"/configure --enable-maintainer-mode --enable-gtk-doc --with-gtk=3.0 --enable-vala ${1+"$@"}
 fi
 
