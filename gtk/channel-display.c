1bcb20fd74502642bc057b16058c89aa43a4b819
Søren Sandmann Pedersen <ssp@redhat.com>
2012-09-02 16:38:28 -0400

    Advertise SPICE_DISPLAY_CAP_A8_SURFACE

--- gtk/channel-display.c
+++ gtk/channel-display.c
@@ -682,6 +682,7 @@ static void spice_display_channel_reset_capabilities(SpiceChannel *channel)
     spice_channel_set_capability(SPICE_CHANNEL(channel), SPICE_DISPLAY_CAP_SIZED_STREAM);
     spice_channel_set_capability(SPICE_CHANNEL(channel), SPICE_DISPLAY_CAP_MONITORS_CONFIG);
     spice_channel_set_capability(SPICE_CHANNEL(channel), SPICE_DISPLAY_CAP_COMPOSITE);
+    spice_channel_set_capability(SPICE_CHANNEL(channel), SPICE_DISPLAY_CAP_A8_SURFACE);
 }
 
 static void spice_display_channel_init(SpiceDisplayChannel *channel)
