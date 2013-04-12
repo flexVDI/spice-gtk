d0668d782300197b68b17a797ca67277ab817083
Christophe Fergeau <cfergeau@redhat.com>
2013-04-12 11:03:52 +0200

    Revert "build-sys: keep common submodule up to date"
    This reverts commit 5150285e1cba35570c29bd923df065d2c81f081e.

--- autogen.sh
+++ autogen.sh
@@ -5,7 +5,7 @@ set -e # exit on errors
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
-git submodule update --init --recursive --remote
+git submodule update --init --recursive
 
 gtkdocize
 autoreconf -v --force --install
