f31bc2a9a6700bebc258130e802f40180af77d8e
Marc-André Lureau <marcandre.lureau@redhat.com>
2013-09-20 17:54:58 +0200

    build-sys: fix g_clear_pointer usage on old glib
    As pointed out and verified on the ML after 0.21 release by Klaus
    Hochlehnert.

--- gtk/channel-cursor.c
+++ gtk/channel-cursor.c
@@ -15,6 +15,7 @@
    You should have received a copy of the GNU Lesser General Public
    License along with this library; if not, see <http://www.gnu.org/licenses/>.
 */
+#include "glib-compat.h"
 #include "spice-client.h"
 #include "spice-common.h"
 
