flexVDI/spice-gtk

commit 5f67178c9602ed43f83f35a6d3097eb137244493

win32: don't block key up events
The win32 ll keyboard hook avoid Windows and other application to
receive global keyboard events. But some key combinations can't be
filtered, such as Win+L. However, the windows lock screen doesn't catch
that the Win key is released, when virt-viewer still holds the hook and
filters it.

So pressing Win+L quickly will lock the screen, but some key press in
the password entry will still be handled as if the Win key was pressed,
such as Win+P or Win+U and probably other, and prevents user from typing
his password.

The only working solution I could find is to just let go all the release
key events in the hook. There doesn't seem any drawback with that.

https://bugzilla.redhat.com/show_bug.cgi?id=917986

gtk/spice-widget.c
@@ -659,7 +659,7 @@ void spice_display_set_grab_keys(SpiceDisplay *display, SpiceGrabSequence *seq)
 #ifdef WIN32
 static LRESULT CALLBACK keyboard_hook_cb(int code, WPARAM wparam, LPARAM lparam)
 {
-    if  (win32_window && code == HC_ACTION) {
+    if  (win32_window && code == HC_ACTION && wparam == WM_KEYDOWN) {
         KBDLLHOOKSTRUCT *hooked = (KBDLLHOOKSTRUCT*)lparam;
         DWORD dwmsg = (hooked->flags << 24) | (hooked->scanCode << 16) | 1;
 

commit 4c7f3914941eb70a0e6060f40d3749c4f6d1424d

win32: fix alt-tab grab regression
Since 5f67178c, alt-tab is no longer grabbed by the client. The keyboard
hook still needs to handle WM_SYSKEY{DOWN,UP} messages.

https://bugzilla.redhat.com/show_bug.cgi?id=873341

gtk/spice-widget.c
@@ -659,7 +659,7 @@ void spice_display_set_grab_keys(SpiceDisplay *display, SpiceGrabSequence *seq)
 #ifdef WIN32
 static LRESULT CALLBACK keyboard_hook_cb(int code, WPARAM wparam, LPARAM lparam)
 {
-    if  (win32_window && code == HC_ACTION && wparam == WM_KEYDOWN) {
+    if  (win32_window && code == HC_ACTION && wparam != WM_KEYUP) {
         KBDLLHOOKSTRUCT *hooked = (KBDLLHOOKSTRUCT*)lparam;
         DWORD dwmsg = (hooked->flags << 24) | (hooked->scanCode << 16) | 1;
 