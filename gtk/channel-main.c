flexVDI/spice-gtk

commit c3adb244259552b6cd528ab901e4d2334ca5b940

channel-main: Reset agent message receive state on agent stop
Discard any partially received messages from the agent on agent stop.

Signed-off-by: Hans de Goede <hdegoede@redhat.com>

gtk/channel-main.c
@@ -1362,11 +1362,8 @@ static void agent_start(SpiceMainChannel *channel)
 /* coroutine context  */
 static void agent_stopped(SpiceMainChannel *channel)
 {
-    SpiceMainChannelPrivate *c = SPICE_MAIN_CHANNEL(channel)->priv;
-
-    set_agent_connected(channel, FALSE);
-    c->agent_caps_received = false;
-    c->agent_display_config_sent = false;
+    spice_main_channel_reset_agent(channel);
+    set_agent_connected(channel, FALSE); /* For notify */
     emit_main_context(channel, SPICE_MAIN_AGENT_UPDATE);
 }
 

commit 0704147d40798d27953ebce6c81578a8c42d20c5

main: fix notify of agent disconnection
spice_main_channel_reset_agent() reset connected state, and prevent
notify of property change.

This is a minor regression introduced in c3adb24425.

gtk/channel-main.c
@@ -375,6 +375,9 @@ static void spice_main_channel_reset(SpiceChannel *channel, gboolean migrating)
     agent_free_msg_queue(SPICE_MAIN_CHANNEL(channel));
     c->agent_msg_queue = g_queue_new();
 
+    /* check: if agent_connected can be TRUE, it should call instead
+       set_agent_connected() to notify new state */
+    g_warn_if_fail(c->agent_connected == FALSE);
     spice_main_channel_reset_agent(SPICE_MAIN_CHANNEL(channel));
 
     SPICE_CHANNEL_CLASS(spice_main_channel_parent_class)->channel_reset(channel, migrating);
@@ -1399,8 +1402,8 @@ static void agent_start(SpiceMainChannel *channel)
 /* coroutine context  */
 static void agent_stopped(SpiceMainChannel *channel)
 {
-    spice_main_channel_reset_agent(channel);
     set_agent_connected(channel, FALSE); /* For notify */
+    spice_main_channel_reset_agent(channel);
     emit_main_context(channel, SPICE_MAIN_AGENT_UPDATE);
 }
 