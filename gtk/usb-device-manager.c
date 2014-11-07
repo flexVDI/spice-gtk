673dabb26bd32f00ea90f358ce70161c3ae73cc6
Marc-André Lureau <marcandre.lureau@redhat.com>
2014-11-07 20:27:16 +0100

    usb: stop processing usb events on error
    If libusb returned an error in the event loop, stop further event
    handling. This avoid spinning in an error loop in error cases.

--- gtk/usb-device-manager.c
+++ gtk/usb-device-manager.c
@@ -1174,6 +1174,7 @@ static gpointer spice_usb_device_manager_usb_ev_thread(gpointer user_data)
         if (rc && rc != LIBUSB_ERROR_INTERRUPTED) {
             const char *desc = spice_usbutil_libusb_strerror(rc);
             g_warning("Error handling USB events: %s [%i]", desc, rc);
+            break;
         }
     }
 
