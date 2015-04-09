4a58187c5eb779b9936eb7b2eb2c6bf032bb2a79
Javier Celaya <javier.celaya@flexvm.es>
2015-04-09 09:52:26 +0200

    Read redirected local ports from command line

--- gtk/spice-cmdline.c
+++ gtk/spice-cmdline.c
@@ -28,6 +28,7 @@ static char *tls_port;
 static char *password;
 static char *uri;
 static GStrv redir_rports;
+static GStrv redir_lports;
 
 static GOptionEntry spice_entries[] = {
     {
@@ -72,6 +73,13 @@ static GOptionEntry spice_entries[] = {
         .description      = N_("Redirect a remote TCP port"),
         .arg_description  = N_("[bind_address:]guest_port:host:hostport"),
     },{
+        .long_name        = "redirect-lport",
+        .short_name       = 'L',
+        .arg              = G_OPTION_ARG_STRING_ARRAY,
+        .arg_data         = &redir_lports,
+        .description      = N_("Redirect a local TCP port"),
+        .arg_description  = N_("[bind_address:]local_port:host:hostport"),
+    },{
         /* end of list */
     }
 };
@@ -105,4 +113,6 @@ void spice_cmdline_session_setup(SpiceSession *session)
         g_object_set(session, "password", password, NULL);
     if (redir_rports)
         g_object_set(session, "redirected-remote-ports", redir_rports, NULL);
+    if (redir_lports)
+        g_object_set(session, "redirected-local-ports", redir_lports, NULL);
 }
