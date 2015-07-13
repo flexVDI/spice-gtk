flexVDI/spice-gtk

commit 4b5e6ec2114e1250c81027ebeac9cfe8d059153f

Send monitor config if at least one monitor has dimensions
If a client (virt-manager, spicy) is not setting display dimensions
and the "resize-guest" property is disabled, spice-gtk sends a wrong
monitor config message where all the monitors have width = heigh = 0
when the agent connects. This message can confuse the guest, in that
case the guest will change the resolution of its monitor.

Regression since 28312b8d1e287a320851e8828825f2ca138d8b0b

Resolves:
https://bugzilla.redhat.com/show_bug.cgi?id=1240721

src/channel-main.c
@@ -1415,6 +1415,22 @@ static void agent_clipboard_release(SpiceMainChannel *channel, guint selection)
     agent_msg_queue(channel, VD_AGENT_CLIPBOARD_RELEASE, msgsize, msg);
 }
 
+static gboolean any_display_has_dimensions(SpiceMainChannel *channel)
+{
+    SpiceMainChannelPrivate *c;
+    guint i;
+
+    g_return_if_fail(SPICE_IS_MAIN_CHANNEL(channel));
+    c = channel->priv;
+
+    for (i = 0; i < MAX_DISPLAY; i++) {
+        if (c->display[i].width > 0 && c->display[i].height > 0)
+            return TRUE;
+    }
+
+    return FALSE;
+}
+
 /* main context*/
 static gboolean timer_set_display(gpointer data)
 {
@@ -1426,6 +1442,11 @@ static gboolean timer_set_display(gpointer data)
     c->timer_id = 0;
     if (!c->agent_connected)
         return FALSE;
+
+    if (!any_display_has_dimensions(channel)) {
+        SPICE_DEBUG("Not sending monitors config, at least one monitor must have dimensions");
+        return FALSE;
+    }
 
     session = spice_channel_get_session(SPICE_CHANNEL(channel));
 

commit f808f09d05ca0d3102f49524594643b53f8fda36

Use g_return_val_if_fail instead of wrong g_return_if_fail
The commit 4b5e6ec2114e1250c81027ebeac9cfe8d059153f introduced a function
returning gboolean, g_return_val_if_fail() should be used instead of
g_return_if_fail().

src/channel-main.c
@@ -1420,7 +1420,7 @@ static gboolean any_display_has_dimensions(SpiceMainChannel *channel)
     SpiceMainChannelPrivate *c;
     guint i;
 
-    g_return_if_fail(SPICE_IS_MAIN_CHANNEL(channel));
+    g_return_val_if_fail(SPICE_IS_MAIN_CHANNEL(channel), FALSE);
     c = channel->priv;
 
     for (i = 0; i < MAX_DISPLAY; i++) {