flexVDI/spice-gtk

commit ea37f06eaa11b6307c797895aeb85d87d142625a

session: Keep brackets around ipv6 hostname
According to rfc2732:
"To use a literal IPv6 address in a URL, the literal address should be
enclosed in "[" and "]" characters."

Resolves: rhbz#1331777

Acked-by: Marc-André Lureau <mlureau@redhat.com>

src/spice-session.c
@@ -476,7 +476,7 @@ static int spice_parse_uri(SpiceSession *session, const char *original_uri)
         }
         tmp[0] = '\0';
         tmp++;
-        host = g_strdup(authority + 1);
+        host = g_strdup_printf("[%s]", authority + 1);
         if (tmp[0] == ':')
             port = g_strdup(tmp + 1);
     } else {

commit 5a301b201be77c0ece71640ba99e92733a93f950

session: Fix IPv6 by using g_network_address_parse
It supports quoting the address with []

Regression since ea37f06eaa11b6307c797895aeb85d87d142625a

Fixes:
https://bugzilla.redhat.com/show_bug.cgi?id=1361478

src/spice-session.c
@@ -2116,7 +2116,7 @@ static gboolean open_host_idle_cb(gpointer data)
 #endif
         } else {
             SPICE_DEBUG("open host %s:%d", s->host, open_host->port);
-            address = g_network_address_new(s->host, open_host->port);
+            address = g_network_address_parse(s->host, open_host->port, &open_host->error);
         }
 
         if (address == NULL || open_host->error != NULL) {