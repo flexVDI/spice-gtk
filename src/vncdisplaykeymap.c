flexVDI/spice-gtk

commit 9ff67a108bb76e50a2c85a5054e6521088df1800

vncdisplaykeymap: Remove obsolete GTK+ 2 compatibility macros
Signed-off-by: Francois Gouget <fgouget@codeweavers.com>
Acked-by: Jonathon Jongsma <jjongsma@redhat.com>

src/vncdisplaykeymap.c
@@ -45,11 +45,6 @@
  */
 
 
-/* Compatability code to allow build on Gtk2 and Gtk3 */
-#ifndef GDK_Tab
-#define GDK_Tab GDK_KEY_Tab
-#endif
-
 /* keycode translation for sending ISO_Left_Send
  * to vncserver
  */
@@ -57,7 +52,7 @@ static struct {
 	GdkKeymapKey *keys;
 	gint n_keys;
 	guint keyval;
-} untranslated_keys[] = {{NULL, 0, GDK_Tab}};
+} untranslated_keys[] = {{NULL, 0, GDK_KEY_Tab}};
 
 static unsigned int ref_count_for_untranslated_keys = 0;
 
@@ -87,41 +82,21 @@ static unsigned int ref_count_for_untranslated_keys = 0;
 /* Xorg Cygwin aka XWin (offset + mangled XT keycodes) */
 #include "vncdisplaykeymap_xorgxwin2xtkbd.c"
 
-/* Gtk2 compat */
-#ifndef GDK_IS_X11_WINDOW
-#define GDK_IS_X11_WINDOW(win) (1)
-#endif
 #endif
 
 #ifdef GDK_WINDOWING_WIN32
 /* Win32 native virtual keycodes */
 #include "vncdisplaykeymap_win322xtkbd.c"
-
-/* Gtk2 compat */
-#ifndef GDK_IS_WIN32_WINDOW
-#define GDK_IS_WIN32_WINDOW(win) (1)
-#endif
 #endif
 
 #ifdef GDK_WINDOWING_QUARTZ
 /* OS-X native keycodes */
 #include "vncdisplaykeymap_osx2xtkbd.c"
-
-/* Gtk2 compat */
-#ifndef GDK_IS_QUARTZ_WINDOW
-#define GDK_IS_QUARTZ_WINDOW(win) (1)
-#endif
 #endif
 
 #ifdef GDK_WINDOWING_BROADWAY
 /* X11 keysyms */
 #include "vncdisplaykeymap_x112xtkbd.c"
-
-/* Gtk2 compat */
-#ifndef GDK_IS_BROADWAY_WINDOW
-#define GDK_IS_BROADWAY_WINDOW(win) (1)
-#endif
-
 #endif
 
 #ifdef GDK_WINDOWING_X11

commit 8b47fe9ef4fd152b87f1e94566c0ed147ffbe812

vncdisplaykeymap: Include missing headers
Causing build failures on win32
 .libs/vncdisplaykeymap.o: In function `vnc_display_keymap_gdk2xtkbd_table':
  /builddir/build/BUILD/spice-gtk-0.33.19-efbfd/build_win32/src/../../src/vncdisplaykeymap.c:198: undefined reference to `GDK_IS_WIN32_WINDOW'
 collect2: error: ld returned 1 exit status

Regression from 9ff67a108bb76e50a2c85a5054e6521088df1800
Acked-by: Frediano Ziglio <fziglio@redhat.com>

src/vncdisplaykeymap.c
@@ -85,11 +85,15 @@ static unsigned int ref_count_for_untranslated_keys = 0;
 #endif
 
 #ifdef GDK_WINDOWING_WIN32
+#include <gdk/gdkwin32.h>
+
 /* Win32 native virtual keycodes */
 #include "vncdisplaykeymap_win322xtkbd.c"
 #endif
 
 #ifdef GDK_WINDOWING_QUARTZ
+#include <gdk/gdkquartz.h>
+
 /* OS-X native keycodes */
 #include "vncdisplaykeymap_osx2xtkbd.c"
 #endif