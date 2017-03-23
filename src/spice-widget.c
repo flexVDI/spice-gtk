flexVDI/spice-gtk

commit ef7a6fa1798c8e53c0b77330b398a78181cf90e5

gtk: Avoid deprecated gdk_pointer_grab
gdk_pointer_grab() was deprecated in 3.0 for gdk_device_grab()
but that was also deprecated for gdk_seat_grab() in 3.20

Acked-by: Victor Toso <victortoso@redhat.com>
Acked-by: Christophe Fergeau <cfergeau@redhat.com>

src/spice-widget.c
@@ -1031,6 +1031,17 @@ static gboolean do_pointer_grab(SpiceDisplay *display)
 #endif
 
     try_keyboard_grab(display);
+    G_GNUC_BEGIN_IGNORE_DEPRECATIONS
+#if GTK_CHECK_VERSION(3, 20, 0)
+    status = gdk_seat_grab(spice_display_get_default_seat(display),
+                           window,
+                           GDK_SEAT_CAPABILITY_ALL_POINTING,
+                           TRUE,
+                           blank,
+                           NULL,
+                           NULL,
+                           NULL);
+#else
     /*
      * from gtk-vnc:
      * For relative mouse to work correctly when grabbed we need to
@@ -1040,8 +1051,6 @@ static gboolean do_pointer_grab(SpiceDisplay *display)
      * what window the pointer is actally over, so use 'FALSE' for
      * 'owner_events' parameter
      */
-    /* FIXME: gdk_pointer_grab() is deprecated */
-    G_GNUC_BEGIN_IGNORE_DEPRECATIONS
     status = gdk_pointer_grab(window, FALSE,
                      GDK_POINTER_MOTION_MASK |
                      GDK_BUTTON_PRESS_MASK |
@@ -1051,6 +1060,7 @@ static gboolean do_pointer_grab(SpiceDisplay *display)
                      NULL,
                      blank,
                      GDK_CURRENT_TIME);
+#endif
     G_GNUC_END_IGNORE_DEPRECATIONS
     grab_successful = (status == GDK_GRAB_SUCCESS);
     if (!grab_successful) {
@@ -1151,9 +1161,6 @@ static void mouse_wrap(SpiceDisplay *display, GdkEventMotion *motion)
 
 }
 
-/* FIXME: gdk_pointer_ungrab() is deprecated */
-G_GNUC_BEGIN_IGNORE_DEPRECATIONS
-
 static void try_mouse_ungrab(SpiceDisplay *display)
 {
     SpiceDisplayPrivate *d = display->priv;
@@ -1164,7 +1171,13 @@ static void try_mouse_ungrab(SpiceDisplay *display)
     if (!d->mouse_grab_active)
         return;
 
+    G_GNUC_BEGIN_IGNORE_DEPRECATIONS
+#if GTK_CHECK_VERSION(3, 20, 0)
+    gdk_seat_ungrab(spice_display_get_default_seat(display));
+#else
     gdk_pointer_ungrab(GDK_CURRENT_TIME);
+#endif
+    G_GNUC_END_IGNORE_DEPRECATIONS
     gtk_grab_remove(GTK_WIDGET(display));
 #ifdef G_OS_WIN32
     ClipCursor(NULL);
@@ -1188,7 +1201,6 @@ static void try_mouse_ungrab(SpiceDisplay *display)
     g_signal_emit(display, signals[SPICE_DISPLAY_MOUSE_GRAB], 0, false);
     spice_gtk_session_set_pointer_grabbed(d->gtk_session, false);
 }
-G_GNUC_END_IGNORE_DEPRECATIONS
 
 static void update_mouse_grab(SpiceDisplay *display)
 {
@@ -2034,7 +2046,11 @@ static gboolean button_event(GtkWidget *widget, GdkEventButton *button)
         */
         /* FIXME: gdk_pointer_ungrab() is deprecated */
         G_GNUC_BEGIN_IGNORE_DEPRECATIONS
+#if GTK_CHECK_VERSION(3, 20, 0)
+        gdk_seat_ungrab(spice_display_get_default_seat(display));
+#else
         gdk_pointer_ungrab(GDK_CURRENT_TIME);
+#endif
         G_GNUC_END_IGNORE_DEPRECATIONS
     }
 

commit a718aec66658ba6bb3bb45a9af81a3aa2a652d18

widget: Use deprecated function to ungrab device
Avoid broken grab (alt+tab not working), regression since
 ef7a6fa1798c8e53c0b77330b398a78181cf90e5

Currently it is not possible to ungrab just keyboard/mouse
using GdkSeat functions:
 https://bugzilla.gnome.org/show_bug.cgi?id=780133
Use deprecated gdk_device_ungrab to do it

Acked-by: Snir Sheriber <ssheribe@redhat.com>

src/spice-widget.c
@@ -890,6 +890,20 @@ static void try_keyboard_grab(SpiceDisplay *display)
     }
 }
 
+static void ungrab_keyboard(G_GNUC_UNUSED SpiceDisplay *display)
+{
+    G_GNUC_BEGIN_IGNORE_DEPRECATIONS
+#if GTK_CHECK_VERSION(3, 20, 0)
+    /* we want to ungrab just the keyboard - it is not possible using gdk_seat_ungrab().
+       See also https://bugzilla.gnome.org/show_bug.cgi?id=780133 */
+    GdkDevice *keyboard = gdk_seat_get_keyboard(spice_display_get_default_seat(display));
+    gdk_device_ungrab(keyboard, GDK_CURRENT_TIME);
+#else
+    gdk_keyboard_ungrab(GDK_CURRENT_TIME);
+#endif
+    G_GNUC_END_IGNORE_DEPRECATIONS
+}
+
 static void try_keyboard_ungrab(SpiceDisplay *display)
 {
     SpiceDisplayPrivate *d = display->priv;
@@ -899,11 +913,7 @@ static void try_keyboard_ungrab(SpiceDisplay *display)
         return;
 
     SPICE_DEBUG("ungrab keyboard");
-#if GTK_CHECK_VERSION(3, 20, 0)
-    gdk_seat_ungrab(spice_display_get_default_seat(display));
-#else
-    gdk_keyboard_ungrab(GDK_CURRENT_TIME);
-#endif
+    ungrab_keyboard(display);
 #ifdef G_OS_WIN32
     // do not use g_clear_pointer as Windows API have different linkage
     if (d->keyboard_hook) {
@@ -1161,6 +1171,20 @@ static void mouse_wrap(SpiceDisplay *display, GdkEventMotion *motion)
 
 }
 
+static void ungrab_pointer(G_GNUC_UNUSED SpiceDisplay *display)
+{
+    G_GNUC_BEGIN_IGNORE_DEPRECATIONS
+#if GTK_CHECK_VERSION(3, 20, 0)
+    /* we want to ungrab just the pointer - it is not possible using gdk_seat_ungrab().
+       See also https://bugzilla.gnome.org/show_bug.cgi?id=780133 */
+    GdkDevice *pointer = gdk_seat_get_pointer(spice_display_get_default_seat(display));
+    gdk_device_ungrab(pointer, GDK_CURRENT_TIME);
+#else
+    gdk_pointer_ungrab(GDK_CURRENT_TIME);
+#endif
+    G_GNUC_END_IGNORE_DEPRECATIONS
+}
+
 static void try_mouse_ungrab(SpiceDisplay *display)
 {
     SpiceDisplayPrivate *d = display->priv;
@@ -1171,13 +1195,7 @@ static void try_mouse_ungrab(SpiceDisplay *display)
     if (!d->mouse_grab_active)
         return;
 
-    G_GNUC_BEGIN_IGNORE_DEPRECATIONS
-#if GTK_CHECK_VERSION(3, 20, 0)
-    gdk_seat_ungrab(spice_display_get_default_seat(display));
-#else
-    gdk_pointer_ungrab(GDK_CURRENT_TIME);
-#endif
-    G_GNUC_END_IGNORE_DEPRECATIONS
+    ungrab_pointer(display);
     gtk_grab_remove(GTK_WIDGET(display));
 #ifdef G_OS_WIN32
     ClipCursor(NULL);
@@ -2044,14 +2062,7 @@ static gboolean button_event(GtkWidget *widget, GdkEventButton *button)
            FIXME: should be multiple widget grab, but how?
            or should know the position of the other widgets?
         */
-        /* FIXME: gdk_pointer_ungrab() is deprecated */
-        G_GNUC_BEGIN_IGNORE_DEPRECATIONS
-#if GTK_CHECK_VERSION(3, 20, 0)
-        gdk_seat_ungrab(spice_display_get_default_seat(display));
-#else
-        gdk_pointer_ungrab(GDK_CURRENT_TIME);
-#endif
-        G_GNUC_END_IGNORE_DEPRECATIONS
+        ungrab_pointer(display);
     }
 
     if (!d->inputs)