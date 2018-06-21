a45e8a56e389e41c891eaa204b16dd89e74e2e69
Frediano Ziglio <fziglio@redhat.com>
2018-06-21 06:52:56 +0100

    Fix build for newer LibreSSL
    LibreSSL 2.7 hidden some definition, like OpenSSL 1.1.
    This fixes https://bugs.freebsd.org/bugzilla/show_bug.cgi?id=229044.
    Original patch from Piotr Kubaj.
    
    Reported-by: Jack L. <xxjack12xx@gmail.com>
    Signed-off-by: Frediano Ziglio <fziglio@redhat.com>
    Acked-by: Christophe Fergeau <cfergeau@redhat.com>

--- src/spice-channel.c
+++ src/spice-channel.c
@@ -55,7 +55,8 @@ static void spice_channel_reset_capabilities(SpiceChannel *channel);
 static void spice_channel_send_migration_handshake(SpiceChannel *channel);
 static gboolean channel_connect(SpiceChannel *channel, gboolean tls);
 
-#if OPENSSL_VERSION_NUMBER < 0x10100000 || defined(LIBRESSL_VERSION_NUMBER)
+#if OPENSSL_VERSION_NUMBER < 0x10100000 || \
+    (defined(LIBRESSL_VERSION_NUMBER) && LIBRESSL_VERSION_NUMBER < 0x20700000)
 static RSA *EVP_PKEY_get0_RSA(EVP_PKEY *pkey)
 {
     if (pkey->type != EVP_PKEY_RSA) {
